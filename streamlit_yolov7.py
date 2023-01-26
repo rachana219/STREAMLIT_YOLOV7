68090c9b37f7c8fe7bcd89bbc5c7268ec741bce5
rachana219 <singhrach@gmail.com>
2023-01-26 13:24:50 +0000

    updated new weights

--- streamlit_yolov7.py
+++ streamlit_yolov7.py
@@ -145,14 +145,14 @@ class Streamlit_YOLOV7(SingleInference_YOLOV7):
             for item in self.predicted_bboxes_PascalVOC:
                 name=str(item[0])
                 conf=str(round(100*item[-1],2))
-                df.append(name, item[1], item[2], item[3], item[4], conf)
+                df.append({'name':name, 'x0': item[1],'y0': item[2],'x1': item[3],'y1:' item[4],'score:' conf})
                 self.capt=self.capt+ ' name='+name+' confidence='+conf+'%, '
         st.image(self.img_screen, caption=self.capt, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
         st.table(df)
         self.image=None
     
 
-if __name__=='__main__':
+if __name__=='__main__': 
     app=Streamlit_YOLOV7()
 
     #INPUTS for YOLOV7
